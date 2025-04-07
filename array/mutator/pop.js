a873be4edcd31ad73fcfc6788d9901450f3b5abe
nimboo-zrad <nimazr800@gmail.com>
2025-04-07 20:55:02 +0330

    added mutator methods

---
+++ array/mutator/pop.js
@@ -0,0 +1,9 @@
+const arr = ['nima', 'parsa', 'omid', 'ali'];
+const secondArr = ['shiva', 'zohre'];
+
+const newArr = arr.pop();
+console.log('Deleted the last name from array and returns it: ', newArr);
+console.log('the modified array: ', arr);
+
+const newSecArr = secondArr.pop();
+console.log('this method only removes the last element: ', secondArr);
---
+++ array/mutator/push.js
@@ -0,0 +1,9 @@
+const arr = ['nima', 'parsa', 'omid', 'ali'];
+const secondArr = ['shiva', 'zohre'];
+
+arr.push('mehdi');
+console.log('Added mehdi to the array: ', arr);
+
+arr.push(secondArr);
+console.log('Added secondArr to the arr: ', arr);
+
